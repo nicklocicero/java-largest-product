a2c26905af54236841c4760622f6b85191bf3a71
nicklocicero <nicklocicero@gmail.com>
2018-06-14 14:15:10 -0600

    Replaced basic lambda notation with method references.

--- src/edu/cnm/deepdive/product/Main.java
+++ src/edu/cnm/deepdive/product/Main.java
@@ -4,25 +4,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.function.Function;
 import java.util.function.ToIntFunction;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		  Path path = Paths.get("largest-product-data.txt");
-		  Pattern pattern = Pattern.compile("\\s+");
-		  int[][] data = readIntMatrix(path,pattern);
-		}
 
-		private static int[][] readIntMatrix(Path source, Pattern delimiter) throws IOException {
-		  return Files.lines(source)
-		        .map((line) -> delimiter.splitAsStream(line)
-		        		.mapToInt(value -> Integer.parseInt(value))
-		        		.toArray())
-		        .collect(Collectors.toList())
-		        .toArray(new int[0][]);
-		}
+  public static void main(String[] args) throws IOException {
+    Path path = Paths.get("largest-product-data.txt");
+    Pattern pattern = Pattern.compile("\\s+");
+    int[][] data = readIntMatrix(path,pattern);
+    for (int[] d: data) {
+    	System.out.println(Arrays.toString(d));
+    }
+  }
+
+  private static int[][] readIntMatrix(Path source, Pattern delimiter) throws IOException {
+	return Files.lines(source)
+	  .map((line) -> delimiter.splitAsStream(line)
+	    .mapToInt(Integer::parseInt)
+		.toArray())
+	  .toArray(int[][]::new);
+  }
 }
